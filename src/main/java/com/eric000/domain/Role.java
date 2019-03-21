fd9feded87d53566d29d3d994c2ad9d5550900e6
Z <374109876@qq.com>
2019-03-21 17:33:18 +0800

    day1

---
+++ src/main/java/com/eric000/domain/Role.java
@@ -0,0 +1,20 @@
+package com.eric000.domain;
+
+import lombok.Data;
+
+@Data
+public class Role {
+
+    private int id;
+
+    private String name;
+
+    public Role(){
+
+    }
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/eric000/hismaplestory/HismaplestoryApplication.java
@@ -0,0 +1,13 @@
+package com.eric000.hismaplestory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HismaplestoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HismaplestoryApplication.class, args);
+    }
+
+}
